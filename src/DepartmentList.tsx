b9e08fa1244f6c41893f04d3cc3fd628b3b66768
unknown <hetarth002@gmail.com>
2023-05-15 15:25:21 +0530

    add departments

---
+++ src/DepartmentList.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { List, ListItem, ListItemText, Collapse } from "@mui/material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+
+interface Department {
+  id: number;
+  name: string;
+  subDepartments: SubDepartment[];
+}
+
+interface SubDepartment {
+  id: number;
+  name: string;
+}
+
+const departmentData: Department[] = [
+  {
+    id: 1,
+    name: "Customer Service",
+    subDepartments: [
+      { id: 1, name: "Support" },
+      { id: 2, name: "Customer Success" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Design",
+    subDepartments: [
+      { id: 4, name: "Graphic Design" },
+      { id: 5, name: "Product Design" },
+      { id: 6, name: "Web Design" },
+    ],
+  },
+];
+
+const DepartmentList = () => {
+  const [expandedDepartments, setExpandedDepartments] = useState<number[]>([]);
+  const [selectedSubDepartments, setSelectedSubDepartments] = useState<
+    number[]
+  >([]);
+
+  const handleDepartmentToggle = (departmentId: number) => {
+    setExpandedDepartments((prevExpanded) => {
+      if (prevExpanded.includes(departmentId)) {
+        return prevExpanded.filter((id) => id !== departmentId);
+      } else {
+        return [...prevExpanded, departmentId];
+      }
+    });
+  };
+
+  const handleSubDepartmentSelect = (subDepartmentId: number) => {
+    setSelectedSubDepartments((prevSelected) => {
+      if (prevSelected.includes(subDepartmentId)) {
+        return prevSelected.filter((id) => id !== subDepartmentId);
+      } else {
+        return [...prevSelected, subDepartmentId];
+      }
+    });
+  };
+
+  const isDepartmentExpanded = (departmentId: number) => {
+    return expandedDepartments.includes(departmentId);
+  };
+
+  const isSubDepartmentSelected = (subDepartmentId: number) => {
+    return selectedSubDepartments.includes(subDepartmentId);
+  };
+
+  return (
+    <List>
+      {departmentData.map((department) => (
+        <div key={department.id}>
+          <ListItem
+            button
+            onClick={() => handleDepartmentToggle(department.id)}
+          >
+            <ListItemText primary={department.name} />
+            {isDepartmentExpanded(department.id) ? (
+              <ExpandLess />
+            ) : (
+              <ExpandMore />
+            )}
+          </ListItem>
+          <Collapse
+            in={isDepartmentExpanded(department.id)}
+            timeout="auto"
+            unmountOnExit
+          >
+            <List component="div" disablePadding>
+              {department.subDepartments.map((subDepartment) => (
+                <ListItem
+                  key={subDepartment.id}
+                  button
+                  onClick={() => handleSubDepartmentSelect(subDepartment.id)}
+                  style={{ paddingLeft: "20px" }}
+                  selected={isSubDepartmentSelected(subDepartment.id)}
+                >
+                  <ListItemText primary={subDepartment.name} />
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
+        </div>
+      ))}
+    </List>
+  );
+};
+
+export default DepartmentList;
--- src/TableData.tsx
+++ src/TableData.tsx
@@ -1,5 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import DepartmentList from "./DepartmentList";
 
 interface Post {
   userId: number;
@@ -12,34 +21,37 @@ const TableData = () => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data));
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>User ID</TableCell>
-            <TableCell>ID</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Body</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {posts.map(post => (
-            <TableRow key={post.id}>
-              <TableCell>{post.userId}</TableCell>
-              <TableCell>{post.id}</TableCell>
-              <TableCell>{post.title}</TableCell>
-              <TableCell>{post.body}</TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User ID</TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Body</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {posts.map((post) => (
+              <TableRow key={post.id}>
+                <TableCell>{post.userId}</TableCell>
+                <TableCell>{post.id}</TableCell>
+                <TableCell>{post.title}</TableCell>
+                <TableCell>{post.body}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <DepartmentList />
+    </div>
   );
 };
 
